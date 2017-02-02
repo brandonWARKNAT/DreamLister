758d3be4c07a3ba4ee8260da670b802173e540b4
Brandon Uriel Pérez <brandon_break@live.com.mx>
2017-02-02 13:58:19 -0600

    Adding XCode project and README.md

---
+++ DreamLister/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DreamLister
+//
+//  Created by Brandon Uriel Pérez Ramírez on 2/2/17.
+//  Copyright © 2017 Brandon Uriel Pérez Ramírez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
