428c0682033764dc63643660b161f28ae193277d
BrandonWARKNAT <brandon_break@live.com.mx>
2017-02-03 00:41:58 -0600

    Adding UI to application and working with swift extensions

--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  DreamLister
 //
 //  Created by Brandon Uriel Pérez Ramírez on 2/2/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
