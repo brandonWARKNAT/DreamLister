4518efb2ef1f68f5cb73549b88cfcaa53711c6b0
BrandonWARKNAT <brandon_break@live.com.mx>
2017-02-06 20:38:38 -0600

    Fetching results in main view

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,25 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by robot-boy on 06/02/2017.
+//  Copyright © 2017 Brandon Uriel Pérez Ramírez. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+    func configureCell(item: Item){
+        
+        title.text = item.title
+        price.text = "$\(item.price)"
+        details.text = item.details
+    }
+    
+}
